e41cc5e9e222434ec79f98d4652c8276298287b1
Eduard Fernandez <efern218@fiu.edu>
2018-10-14 23:33:26 -0400

    Complete base Framework

--- client/src/components/mainPage/City.js
+++ client/src/components/mainPage/City.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
 import Category from '../eventList.js/Category'
 import axios from 'axios'
-
+import Loader from '../Loader'
 export default class City extends Component {
     constructor() {
         super();
         this.state = {
-            events: []
+            events: [],
+            images: []
         }
     }
     async componentDidMount() {
         const { data } = await axios.get(`/events/${this.props.cityName}`)
         this.setState({ events: data })
+        const { data: images } = await axios.get(`/img/${this.props.cityName}`)
+        this.setState({ images })
     }
     render() {
         const catagories = this.state.events[0]
-        console.log(catagories);
-
-        return (
-
-            <React.Fragment>
-                <h1>Hot Spots!</h1>
-                {/* {catagories.map((e, i) => {
-                    return <Category events={e} key={i} />
-                })} */}
-            </React.Fragment>
-        )
+        const images = this.state.images[0]
+        if (images && catagories) {
+            return (
+                <React.Fragment>
+                    <h1>Hot Spots!</h1>
+                    {catagories.events.event.map((e, i) => {
+                        return <Category events={e} cityName={this.props.cityName} images={images.results[i].links} key={i} />
+                    })}
+                </React.Fragment>
+            )
+        } else {
+            return <Loader />
+        }
     }
 }
