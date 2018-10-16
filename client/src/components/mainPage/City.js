4e754e86071363964c904840bd4914710ebff3d1
Eduard Fernandez <efern218@fiu.edu>
2018-10-16 10:59:54 -0400

    'Add error detector if no Events available'

--- client/src/components/mainPage/City.js
+++ client/src/components/mainPage/City.js
@@ -12,7 +12,6 @@ export default class City extends Component {
     }
     async componentDidMount() {
         if (this.props.category) {
-            console.log(this.state.category)
             const { data } = await axios.get(`/events/${this.props.cityName}/category/${this.props.category}`)
             const { data: images } = await axios.get(`/img/${this.props.category}`)
             this.setState({ events: data, images })
@@ -25,12 +24,12 @@ export default class City extends Component {
     render() {
         const butt = this.state.events[0]
         const images = this.state.images[0]
-        if (images && butt) {
+        if (images && butt.events !== null) {
             return (
                 <React.Fragment>
                     <h1>Hot Spots!</h1>
                     {butt.events.event.map((e, i) => {
-                        return <Category events={e} cityName={this.props.cityName[0]} images={images.results[i].urls} key={i} />
+                        return <Category events={e} cityName={this.props.cityName} images={images.results[i].urls} key={i} />
                     })}
                 </React.Fragment>
             )
