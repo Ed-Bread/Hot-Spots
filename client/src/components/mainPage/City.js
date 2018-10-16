87f7e471ca7f1cf6f822f472030d90786ab75ff9
Adam Wachsberg <awachsberg@gmail.com>
2018-10-16 10:00:17 -0400

    fix variable bug

--- client/src/components/mainPage/City.js
+++ client/src/components/mainPage/City.js
@@ -29,7 +29,7 @@ export default class City extends Component {
             return (
                 <React.Fragment>
                     <h1>Hot Spots!</h1>
-                    {catagories.events.event.map((e, i) => {
+                    {butt.events.event.map((e, i) => {
                         return <Category events={e} cityName={this.props.cityName} images={images.results[i].urls} key={i} />
                     })}
                 </React.Fragment>
