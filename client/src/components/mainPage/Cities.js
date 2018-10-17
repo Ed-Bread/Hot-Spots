75a1ee1d04260d174e383c66b002b8b586c8da07
Eduard Fernandez <efern218@fiu.edu>
2018-10-17 14:04:39 -0400

    Fix Go button and Link set up

--- client/src/components/mainPage/Cities.js
+++ client/src/components/mainPage/Cities.js
@@ -33,9 +33,12 @@ export default class Cities extends Component {
                 <div>
                     <nav className="navbar navbar-light navbar-expand-md navigation-clean">
                         <div className='container'>
-                            <Link className="nav-link" to="/">
-                                <a className="navbar-brand black">HS</a><button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
-                            </Link>
+                            <div className="headerBar">
+                                <Link className="nav-link" to="/">
+                                    <a className="navbar-brand black">HS</a>
+                                </Link>
+                                <button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+                            </div>
                             <div className="collapse navbar-collapse" id="navcol-1">
                                 <ul className="nav navbar-nav ml-auto">
                                     <li className='nav-item' role="presentation" onClick={() => { this.setCity('miami') }}>
