58d1163dbeced54885b0e39ba8c906f27e1ff29a
Eduard Fernandez <42663507+Bravo-Bear@users.noreply.github.com>
2018-10-17 12:32:37 -0400

    Add home button

--- client/src/components/mainPage/Cities.js
+++ client/src/components/mainPage/Cities.js
@@ -32,7 +32,10 @@ export default class Cities extends Component {
 
                 <div>
                     <nav className="navbar navbar-light navbar-expand-md navigation-clean">
-                        <div className='container'><a className="navbar-brand">HS</a><button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+                        <div className='container'>
+                           <Link className="nav-link" to="/miami">
+                                <a className="navbar-brand">HS</a><button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+                            </Link>
                             <div className="collapse navbar-collapse" id="navcol-1">
                               <ul className="nav navbar-nav ml-auto">
                                     <li className='nav-item'role="presentation" onClick={() => { this.setCity('miami') }}>
