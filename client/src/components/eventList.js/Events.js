43b9796358ee8478943f43017220de76c8fbd17e
Eduard Fernandez <efern218@fiu.edu>
2018-10-17 11:58:28 -0400

    Fix merge conflicts

--- client/src/components/eventList.js/Events.js
+++ client/src/components/eventList.js/Events.js
@@ -7,11 +7,13 @@ export default class Events extends Component {
     render() {
         return (
             <div className="event">
-                <p>{this.props.events.description ? this.props.events.description : 'No Description Avaliable'}</p>
+                <p>{this.props.events.description ? `${this.props.events.description.substring(0, 400)}...` : 'No Description Avaliable'}</p>
                 <p>Get more information <a href={this.props.events.venue_url}>here!</a></p>
                 <h4>Location:{this.props.events.venue_address},{this.props.events.city_name}, {this.props.events.region_name}</h4>
-                <MapContainer address={this.props.events.venue_address} city={this.props.events.city_name} region={this.props.events.region_name} title={this.props.events.title} long={this.props.events.longitude} lat={this.props.events.latitude} />
+                <div className="map">
+                    <MapContainer address={this.props.events.venue_address} city={this.props.events.city_name} region={this.props.events.region_name} title={this.props.events.title} long={this.props.events.longitude} lat={this.props.events.latitude} />
+                </div>
             </div>
         )
     }
-  }
+}
--- client/src/components/eventList.js/MapContainer.js
+++ client/src/components/eventList.js/MapContainer.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 export class MapContainer extends Component {
- state = {
-    showingInfoWindow: false,
-    activeMarker: {}
-  };
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {}
+  };
 
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
 
   render() {
     const style = {
-      width: '60%',
-      height: '60%',
-
+      width: '85%',
+      height: '55%',
     }
     return (
 
@@ -31,50 +29,20 @@ export class MapContainer extends Component {
         }}
         style={style}>
         <Marker name={'Current location'}
-                onClick={this.onMarkerClick}/>
-          <InfoWindow
-                    marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}>
-                      <div>
-                        <h1>{this.props.title}</h1>
-                        <address>
-                          {this.props.address}<br/>
-                          {this.props.city}, {this.props.region}
-                        </address>
-                      </div>
-                  </InfoWindow>
+          onClick={this.onMarkerClick} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h5>{this.props.title}</h5>
+            <address>
+              {this.props.address}<br />
+              {this.props.city}, {this.props.region}
+            </address>
+          </div>
+        </InfoWindow>
 
       </Map>
-=======
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-import "../../css/Events.css"
-
-export class MapContainer extends Component {
-
-  componentDidMount() {
-    let foo = document.getElementById("g-map").firstChild
-    foo.id = "mapy"
-    console.log(foo)
-}
-
-  render() {
-    const style = {
-      width: '10px',
-      height: '200px',
-
-    }
-    return (
-      <div id="g-map">
-        <Map google={this.props.google} zoom={14}
-          initialCenter={{
-            lat: this.props.lat,
-            lng: this.props.long,
-          }}
-          >
-          <Marker name={'Current location'} />
-        </Map>
-      </div>
->>>>>>> b1659650006b4b6727ba164630742c69ef09b4bc
 
     );
   }
