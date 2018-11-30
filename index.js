32f8ff94bdc29977621708d5aa067841e059a9de
Eduard Fernandez <42663507+Bravo-Bear@users.noreply.github.com>
2018-11-30 18:22:55 -0500

    Fix Security

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ app.use(express.static(__dirname + '/client/build'));
 
 app.get('/events/:city', async (req, res) => {
     const events = [];
-    let { data } = await axios.get(`http://api.eventful.com/json/events/search?app_key=rtwRNJGjF32csPmx&?...&location=${req.params.city}&date=today`)
+    let { data } = await axios.get(`http://api.eventful.com/json/events/search?app_key=ENV[EVENT_KEY]&?...&location=${req.params.city}&date=today`)
     events.push(data)
     res.json(events)
 })
 app.get('/events/:city/category/:category', async (req, res) => {
     const events = [];
-    let { data } = await axios.get(`http://api.eventful.com/json/events/search?app_key=rtwRNJGjF32csPmx&?...&location=${req.params.city}&category=${req.params.category}&date=today`)
+    let { data } = await axios.get(`http://api.eventful.com/json/events/search?app_key=ENV[EVENT_KEY]&?...&location=${req.params.city}&category=${req.params.category}&date=today`)
     events.push(data)
     res.json(events)
 })
 app.get('/img/:keyword', async (req, res) => {
     const images = [];
     let i = Math.floor(Math.random()*10)
-    let { data } = await axios.get(`https://api.unsplash.com/search/photos?&query=${req.params.keyword}&page=${i}&client_id=e6fc74ee151383d272e0baa254236693152d29360832effd93b02ef2f867dae6`)
+    let { data } = await axios.get(`https://api.unsplash.com/search/photos?&query=${req.params.keyword}&page=${i}&client_id=ENV[IMAGE_KEY]`)
     images.push(data)
     res.send(images)
 })
